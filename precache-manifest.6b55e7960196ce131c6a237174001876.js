ee00da97f6267f9ef28bbd97fc105a1f597289c7
Michael P Garrity <mike@mikegslptop2495.lan>
2020-09-30 14:18:47 -0400

    Updates

---
+++ precache-manifest.6b55e7960196ce131c6a237174001876.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceba16ecb3cdce5d69f94cf44c2cd0ac",
+    "url": "/eisenstein_primes/index.html"
+  },
+  {
+    "revision": "3cf82954144369d1dc59",
+    "url": "/eisenstein_primes/static/css/main.9379ce07.chunk.css"
+  },
+  {
+    "revision": "a45154903d9714a312e9",
+    "url": "/eisenstein_primes/static/js/2.3fc04573.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/eisenstein_primes/static/js/2.3fc04573.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cf82954144369d1dc59",
+    "url": "/eisenstein_primes/static/js/main.8aceee01.chunk.js"
+  },
+  {
+    "revision": "f6ea8847516be0030ca1",
+    "url": "/eisenstein_primes/static/js/runtime-main.90008a86.js"
+  }
+]);
